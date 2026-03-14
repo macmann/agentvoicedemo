4c63df85ca8ee91216b6f4cf8c39216f45bd3fd6
Kaung San Phyoe <davidkaung@gmail.com>
2026-03-14 19:35:50 +0630

    Fix webpack node: scheme import in troubleshooting KB loader

--- orchestration/troubleshootingKb.ts
+++ orchestration/troubleshootingKb.ts
@@ -1,5 +1,5 @@
-import path from "node:path";
-import { readFile } from "node:fs/promises";
+import path from "path";
+import { readFile } from "fs/promises";
 
 export interface TroubleshootingKbSection {
   id: string;
